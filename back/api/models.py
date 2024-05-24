c7cbfb70d32c3cb9ccfa079d3d4aa315f33709e8
Rene Silva <rene.scre@gmail.com>
2024-05-24 12:03:50 -0600

    env file and postgresql stablished

--- back/api/models.py
+++ back/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-'''
+"""
 class Country(models.Model):
     #ISO 3166-1 alfa-3
     iso3 = models.CharField(max_length=3, unique=True)
@@ -16,61 +16,41 @@ class Role(models.Model):
     title = models.CharField(max_length=255, default='')
     description = models.CharField(max_length=255, default='')
 
-'''
+"""
+
+
 class ApiUser(models.Model):
     ROLE_CHOICES = (
-        ('PAC', 'PACIENTE'),
-        ('MED', 'MEDICO'),
-        ('OSA', 'ORGANISMO DE SALUD'),
-        ('OAU', 'ORGANISMO DE AUTENTICACION')
-        )
+        ("PAC", "PACIENTE"),
+        ("MED", "MEDICO"),
+        ("OSA", "ORGANISMO DE SALUD"),
+        ("OAU", "ORGANISMO DE AUTENTICACION"),
+    )
 
-    #ISO 3166-1 alfa-3
+    # ISO 3166-1 alfa-3
     COUNTRY_CHOICES = (
-        ('ARG', 'ARGENTINA'),
-        ('MEX', 'MEXICO'),
-        ('PER', 'PERU'),
-        ('VEN', 'VENEZUELA')
-        )
-    
-    STATUS_CHOICES = (
-        ('P', 'PENDIENTE'),
-        ('A', 'ACTIVO'),
-        ('I', 'INACTIVO')
-        )    
+        ("ARG", "ARGENTINA"),
+        ("MEX", "MEXICO"),
+        ("PER", "PERU"),
+        ("VEN", "VENEZUELA"),
+    )
+
+    STATUS_CHOICES = (("P", "PENDIENTE"), ("A", "ACTIVO"), ("I", "INACTIVO"))
 
     uniqueid = models.CharField(max_length=50, unique=True)
     country_code = models.CharField(choices=COUNTRY_CHOICES, max_length=3)
-    role = models.CharField(choices=ROLE_CHOICES, default='PAC', max_length=3)
-    first_name = models.CharField(max_length=255, default='')
-    last_name = models.CharField(max_length=255, default='')
-    id_type = models.CharField(max_length=255, default='')
-    id_value = models.CharField(max_length=255, default='')
-    genre = models.CharField(max_length=255, default='')
-    civil_status = models.CharField(max_length=255, default='')
+    role = models.CharField(choices=ROLE_CHOICES, default="PAC", max_length=3)
+    first_name = models.CharField(max_length=255, default="")
+    last_name = models.CharField(max_length=255, default="")
+    id_type = models.CharField(max_length=255, default="")
+    id_value = models.CharField(max_length=255, default="")
+    genre = models.CharField(max_length=255, default="")
+    civil_status = models.CharField(max_length=255, default="")
     birth_date = models.DateField(null=True, editable=False)
     password = models.CharField(max_length=255)
-    created_by = models.CharField(max_length=50, default='Usuario', null=True)
+    created_by = models.CharField(max_length=50, default="Usuario", null=True)
     added_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     email = models.EmailField(unique=True)
     notifications = models.BooleanField(default=True)
-    status = models.CharField(choices=STATUS_CHOICES, max_length=1, default='P')
-    
-class MedicData(models.Model):
-    
-    STATUS_CHOICES = (
-        ('P', 'PENDIENTE'),
-        ('A', 'ACTIVO'),
-        ('I', 'INACTIVO')
-        )
-
-    user = models.OneToOneField(ApiUser, on_delete=models.CASCADE)
-    status = models.CharField(choices=STATUS_CHOICES, default='P', max_length=1)
-    proffesional_identifier = models.CharField(null=True, default='', max_length=50)
-    added_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        verbose_name = "MedicData"
-        verbose_name_plural = "MedicData"
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1, default="P")
--- back/salud360/settings.py
+++ back/salud360/settings.py
@@ -11,76 +11,95 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u%g40^7s^3fzoi=idhfz_vmelgkd9va$*1=mww#-4tm=00%z@&'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = []
 
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'api',
-    'debug_toolbar',
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "debug_toolbar",
+]
+
+
+LOCAL_APPS = [
+    "api",
+]
+
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'salud360.urls'
+ROOT_URLCONF = "salud360.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'salud360.wsgi.application'
+WSGI_APPLICATION = "salud360.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
@@ -90,16 +109,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -107,9 +126,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -119,12 +138,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 INTERNAL_IPS = [
     # ...
