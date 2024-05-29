c32890fafed8e3e0bc27839bac2979a305f6fe3a
Yopo Nicolas <nicoyopoit@gmail.com>
2024-05-29 13:38:40 -0300

    Reapply "Back/proyecto base"
    This reverts commit d8eec57f48943471dd833f69549465fb594d7c07.

---
+++ back/api/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+admin.site.register(models.ApiUser)
+admin.site.register(models.Profile)
+admin.site.register(models.MedicData)
+
+
+
