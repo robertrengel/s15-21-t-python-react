0d39ea14cf25a6c5bc36d2fa19081edd043b9945
Melania Palomino <aniapalominoq@gmail.com>
2024-06-04 18:46:46 -0500

    ADD todo los iconos faltantes para exportarlos sin los atributos width, heigth y fill

--- front/src/assets/index.js
+++ front/src/assets/index.js
@@ -1,6 +1,78 @@
 import Logo from "./Logo.svg";
-import Facebook from "./iconos-facebook.svg";
-import Instagram from "./iconos-instagram.svg";
-import Twitter from "./iconos-twitter.svg";
+import Facebook from "./Icono-facebook.svg";
+import Instagram from "./Icono-instagram.svg";
+import Twitter from "./Icono-twitter.svg";
+import Urologia from "./Icono-urologia.svg";
+import Traumatologia from "./Icono-traumatologia.svg";
+import Toxicologia from "./Icono-toxicologia.svg";
+import Telefono from "./Icono-telefono.svg";
+import Sobre from "./Icono-sobre.svg";
+import Perfil from "./Icono-perfil.svg";
+import Pediatra from "./Icono-pediatria.svg";
+import Otorrino from "./Icono-otorrino.svg";
+import Oncologia from "./Icono-oncologia.svg";
+import OjoCerrado from "./Icono-ojo-cerrado.svg";
+import OjoAbierto from "./Icono-ojo-abierto.svg";
+import Oftalmologia from "./Icono-ofatalmologia.svg";
+import Odontologia from "./Icono-odontologia.svg";
+import Neurologia from "./Icono-neurologia.svg";
+import Neumologia from "./Icono-neumologia.svg";
+import Nefrologia from "./Icono-nefrologia.svg";
+import Mundo from "./Icono-mundo.svg";
+import Laboratirio from "./Icono-laboratorio.svg";
+import Home from "./Icono-home.svg";
+import Hemoterapia from "./Icono-hemoterapia.svg";
+import Hematologia from "./Icono-hematologia.svg";
+import Ginecologia from "./Icono-ginecologia.svg";
+import Gastro from "./Icono-gastro.svg";
+import Endocrinologia from "./Icono-endocrinologia.svg";
+import Editar from "./Icono-editar.svg";
+import Dropdown from "./Icono-dropdown.svg";
+import Documento from "./Icono-documento.svg";
+import Dermatologia from "./Icono-dermatologia.svg";
+import Configuracion from "./Icono-configuracion.svg";
+import Comentar from "./Icono-comentar.svg";
+import Buscar from "./Icono-buscar.svg";
+import Error404 from "./Search-amico.png"
+import Error500 from "./server-status.png"
 
-export { Logo, Facebook, Instagram, Twitter };
+export {
+  Logo,
+  Facebook,
+  Instagram,
+  Twitter,
+  Urologia,
+  Traumatologia,
+  Toxicologia,
+  Telefono,
+  Sobre,
+  Perfil,
+  Pediatra,
+  Otorrino,
+  Oncologia,
+  OjoCerrado,
+  OjoAbierto,
+  Oftalmologia,
+  Odontologia,
+  Neurologia,
+  Neumologia,
+  Nefrologia,
+  Mundo,
+  Laboratirio,
+  Home,
+  Hemoterapia,
+  Hematologia,
+  Ginecologia,
+  Gastro,
+  Endocrinologia,
+  Editar,
+  Dropdown,
+  Documento,
+  Dermatologia,
+  Configuracion,
+  Comentar,
+  Buscar,
+Error404,
+Error500
+
+};
