0beae7d622288d59ff6d37f7a03e38f3e6868a9a
Rene Silva <rene.scre@gmail.com>
2024-06-04 21:58:27 -0600

    user country fixed

--- back/api/views.py
+++ back/api/views.py
@@ -2,20 +2,28 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from .models import ApiUser
 from api.serializers import UserCreateSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 
 
+@permission_classes(
+    [
+        IsAuthenticated,
+    ]
+)
 @api_view()
-@permission_classes([IsAuthenticated])
 def user_list(request):
     queryset = ApiUser.objects.all()
     serializer = UserCreateSerializer(queryset, many=True)
     return Response(serializer.data)
 
 
+@permission_classes(
+    [
+        IsAuthenticated,
+    ]
+)
 @api_view()
-@permission_classes([IsAuthenticated])
 def user_detail(request, id):
     profile = get_object_or_404(ApiUser, pk=id)
     serializer = UserCreateSerializer(profile)
