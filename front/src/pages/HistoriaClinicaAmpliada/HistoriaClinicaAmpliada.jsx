a49e887da0421bd216b872d79d918143acacbb48
Melania Palomino <aniapalominoq@gmail.com>
2024-06-07 02:32:25 -0500

    ADD logica para agregar el modal y capturar todos los valores del formulario

--- front/src/pages/HistoriaClinicaAmpliada/HistoriaClinicaAmpliada.jsx
+++ front/src/pages/HistoriaClinicaAmpliada/HistoriaClinicaAmpliada.jsx
@@ -1,79 +1,173 @@
 import styles from "./HistoriaClinicaAmpliada.module.scss";
 import {Input} from "../../components/Input/Input";
 import {Button} from "../../components/Button/Button";
-import {Icons} from "../../components/Icons/Icons"
+import {Icons} from "../../components/Icons/Icons";
+import {ModalForm } from "../../components/ModalForm/ModalForm";
 import {FileUpload} from "../../components/FileUpload/FileUpload";
+import {useState} from "react";
 
 export const HistoriaClinicaAmpliada= ()=>{
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        name: '',
+        apellido: '',
+        edad:'',
+        diagostico: '', 
+        tratamiento: '',
+        comentario: '',
+    });
+
+    const openModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const closeModal = () => {
+      setIsModalOpen(false);
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+        ...formData,
+        [name]: value
+        });
+      };
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+        console.log('Form data:', formData);
+  };
+
+      
 return (
-
-<>
-<section className={styles.section}>
-        <form  action="" className={styles.form}>
-            <div className={styles.form_title}>
-                <h1>Diagnostico</h1>
-                <hr />  
-            </div>
-            <div className={styles.container_fields}>
-                <div className={styles.group}>
-                    <Input type="text" label="Nombre" placeholder="Nombre" />
-                    <Input type="text" label="Apellido" placeholder="Apellido" />
+    <section className={styles.section}>
+            <form  onSubmit={handleSubmit}  className={styles.form}>
+                <div className={styles.form_title}>
+                    <h3>Diagnostico</h3>
                 </div>
-                <div className={styles.group}>
-                    <Input type="date" label="Fecha de Consulta" placeholder="Edad" />
-                </div>
-                    <Input type="textarea" label="Diagnostico" placeholder="input" />
-                    <Input type="textarea" label="Tratamiento" placeholder="input" />
-                    <Input type="textarea" label="Comentarios" placeholder="input" />
-                <div className={styles.btn_carga}>
-                    <FileUpload icon={<Icons icon="documento" size={36} color="green"  />} size={60} />
-                    <Button onClick={()=>{console.alert("boton")}} width={60} height={60} >
-                        <Icons icon="comentar" size={33}   />
-                    </Button>
-                </div>
-                <div className={styles.btn_submit}>
-                <Button   width={159} height={48} onClick={()=>{console.log("boton")}}>Volver</Button>
-                <Button   width={159} height={48} onClick={()=>{console.log("boton")}}>Guardar</Button>
+                <div className={styles.container_fields}>
+                    <div className={styles.group}>
+                        <Input 
+                            type="text" 
+                            label="Nombre"
+                            name="Nombre"
+                            placeholder="Nombre" 
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                        <Input 
+                            type="text" 
+                            label="Apellido" 
+                            placeholder="Apellido" 
+                            name="Apellido"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className={styles.group}>
+                        <Input 
+                            type="date" 
+                            label="Fecha de Consulta" 
+                            placeholder="Edad" 
+                            name="Edad"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+                        <Input 
+                        type="textarea" 
+                        label="Diagnostico" 
+                        placeholder="input" 
+                        name="Diagnostico"
+                        value={formData.name}
+                        onChange={handleChange}
+                        />
+                        <Input 
+                            type="textarea" 
+                            label="Tratamiento" 
+                            placeholder="input" 
+                        />
+                        <Input 
+                            type="textarea" 
+                            label="Comentarios" 
+                            placeholder="input"
+                            value={formData.name}
+                            onChange={handleChange} 
+                        />
+                    <div className={styles.btn_carga}>
+                        <div className={styles.fileUpload} >
+                        <FileUpload 
+                            icon={
+                                <Icons 
+                                    icon="documento" 
+                                    size={36} 
+                                    color="green"  
+                                />} 
+                            size={60} 
+                        />
+                        </div>
+                        <div className={`${styles.modal} ${isModalOpen ? styles.modal_open : ''}`}>
+                            <Button 
+                                onClick={openModal}
+                                width={60} 
+                                height={60}
+                            >
+                                <Icons
+                                    icon="comentar"
+                                    size={33}  
+                                />
+                            </Button>
+                            {isModalOpen && (
+                                <ModalForm 
+                                    title="Comentario" 
+                                    onClose={closeModal}
+                                >
+                                    <Input
+                                        type="textarea"
+                                        name="Comentario"
+                                    />
+                                </ModalForm>
+                            )}
+                        </div>
+                    </div>
+                    <div className={styles.btn_submit}>
+                            <Button   
+                                width={159} 
+                                height={48} 
+                                onClick={()=>{console.log("boton")}}>
+                                Volver
+                            </Button>
+                            <Button   
+                                width={159} 
+                                height={48} 
+                                onClick={()=>{console.log("boton")}}>
+                                Guardar
+                            </Button>
+                        </div>
 
                 </div>
+            </form>
+        <article className={styles.article}>
+        <div className={styles.article_title}>
+            <h3>Historial</h3>
+        </div>
+        <div className={styles.article_cometario}>
+            <time dateTime="2023-06-06" >25/05/2024</time>
+            <p>
+                Marcelo Perez ha agregado un nuevo documento
+            </p> 
+        </div>
+        <div className={styles.article_cometario}>
+            <time dateTime="2023-06-06" >25/05/2024</time>
+            <p>
+                Marcelo Perez ha agregado un nuevo documento
+            </p> 
+        </div>
 
 
-
-            </div>
-        </form>
-    <article className={styles.article}>
-    <div className={styles.article_title}>
-        <h3>Historial</h3>
-        <hr />  
-    </div>
-    <div className={styles.article_cometario}>
-        <time datetime="2023-06-06" >25/05/2024</time>
-        <p>
-            Marcelo Perez ha agregado un nuevo documento
-        </p> 
-    </div>
-    <hr />
-    <div className={styles.article_cometario}>
-        <time datetime="2023-06-06" >25/05/2024</time>
-        <p>
-            Marcelo Perez ha agregado un nuevo documento
-        </p>
-    </div>
-    <hr />
-    <div className={styles.article_cometario}>
-        <time datetime="2023-06-06" >25/05/2024</time>
-        <p>
-            Marcelo Perez ha agregado un nuevo documento
-        </p>
-    </div>
-
-    </article>
-</section>
-
-</>
-
-
+        </article>
+    </section>
 )
 
 
