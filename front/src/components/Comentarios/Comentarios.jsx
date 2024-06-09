a8a86827c9b0ab885f67261f41559fdc08862c9a
Miguel Guimar Salazar <116126244+Soymigueprogramador@users.noreply.github.com>
2024-06-09 20:04:36 -0300

    Sigo con problemas con los estilos, mi problema es que no ocupan el espacio que indica en figma ni tampoco me deja acomodar las cosas como esta en figma, no se si es mi falta de experiencia o que pero me volvio loco

--- front/src/components/Comentarios/Comentarios.jsx
+++ front/src/components/Comentarios/Comentarios.jsx
@@ -1,11 +1,15 @@
 import styles from "./Comentarios.module.scss";
 import { Input } from "../Input/Input";
+import { Button } from "../../components/Button/Button";
 
 export const Comentarios = () => {
   return (
+    <div className={styles.general_container}>
+
     <div className={styles.container_coment}>
+  
       <div className={styles.title_coment}>
-        <h2 className={styles.title}> Marcelo perez </h2>
+        <h2 className={styles.title}>Marcelo Perez</h2>
         <img
           className={styles.img}
           src="../../../public/imagenes/fecha.png"
@@ -16,16 +20,39 @@ export const Comentarios = () => {
           src="../../../public/imagenes/mensaje.png"
           alt="11/10/23"
         />
-        <div className={styles.coment_field}>
+      </div>
+  
+      <div className={styles.coment_field}>
         <div>
-            <Input
-                className={styles.este_textarea}
-                type="textarea"
-                placeholder="Ingresa tu comentario"
-            />
-        </div>
+          <Input
+            className={styles.este_textarea}
+            type="textarea"
+            placeholder="Ingresa tu comentario"
+          />
         </div>
       </div>
+  
+      <div className={styles.container_diagnosis}>
+        <img
+          className={styles.diagnostico}
+          src="../../../public/imagenes/boton-diagnostico.png"
+          alt=""
+        />
+      </div>
+  
+      <div className={styles.button_keep}>
+        <Button width={159} height={48}>
+          <img
+            className={styles.mensaje}
+            src="../../../public/imagenes/mensaje.png"
+            alt="11/10/23"
+          />
+          Guardar
+        </Button>
+      </div> 
+  
     </div>
+  </div>
+
   );
 };
