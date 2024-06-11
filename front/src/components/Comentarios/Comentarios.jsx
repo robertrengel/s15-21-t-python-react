c39b513056e6e561a951ee66b308f39f88ed1c66
Miguel Guimar Salazar <116126244+Soymigueprogramador@users.noreply.github.com>
2024-06-11 13:59:04 -0300

    Eh creado un nuevo componente llamado Label el cual muestra los tags, ya que estos se muestran en varios lugares diferentes del proyecto

--- front/src/components/Comentarios/Comentarios.jsx
+++ front/src/components/Comentarios/Comentarios.jsx
@@ -2,57 +2,55 @@ import styles from "./Comentarios.module.scss";
 import { Input } from "../Input/Input";
 import { Button } from "../../components/Button/Button";
 
-export const Comentarios = () => {
+export const Comentarios = ({
+  userName = "Macerlo Perez",
+  fecha = "11/10/23",
+  mensaje = "Comentario",
+}) => {
   return (
     <div className={styles.general_container}>
-
-    <div className={styles.container_coment}>
-  
-      <div className={styles.title_coment}>
-        <h2 className={styles.title}>Marcelo Perez</h2>
-        <img
-          className={styles.img}
-          src="../../../public/imagenes/fecha.png"
-          alt="11/10/23"
-        />
-        <img
-          className={styles.mensaje}
-          src="../../../public/imagenes/mensaje.png"
-          alt="11/10/23"
-        />
-      </div>
-  
-      <div className={styles.coment_field}>
-        <div>
-          <Input
-            className={styles.este_textarea}
-            type="textarea"
-            placeholder="Ingresa tu comentario"
-          />
+      <div className={styles.container_coment}>
+        <div className={styles.title_coment}>
+          <h2 className={styles.title}>{userName}</h2>
+          <span> {fecha} </span>
+          <p> {mensaje} </p>
         </div>
-      </div>
-  
-      <div className={styles.container_diagnosis}>
+
+        <div className={styles.coment_field}>
+          <div>
+            <Input
+              className={styles.este_textarea}
+              type="textarea"
+              placeholder="Ingresa tu comentario"
+            />
+            <input type="file" className={styles.button_file} />
+            <img
+              className={styles.diagnostico}
+              src="../../../public/imagenes/boton-diagnostico.png"
+              alt=""
+            />
+          </div>
+        </div>
+
+        {/* <div className={styles.container_diagnosis}>
         <img
           className={styles.diagnostico}
           src="../../../public/imagenes/boton-diagnostico.png"
           alt=""
         />
-      </div>
-  
-      <div className={styles.button_keep}>
-        <Button width={159} height={48}>
-          <img
-            className={styles.mensaje}
-            src="../../../public/imagenes/mensaje.png"
-            alt="11/10/23"
-          />
-          Guardar
-        </Button>
-      </div> 
-  
-    </div>
-  </div>
+      </div> */}
 
+        <div className={styles.button_keep}>
+          <Button width={159} height={48}>
+            <img
+              className={styles.mensaje}
+              src="../../../public/imagenes/mensaje.png"
+              alt="11/10/23"
+            />
+            Guardar
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
---
+++ front/src/components/Label/Label.jsx
@@ -0,0 +1,13 @@
+import styles from './Label.module.scss'
+
+export const Label = () => {
+  return (
+    <div className={styles.label_container}>
+      <img src="../../../public/imagenes/chip-profesionales.png" alt="" />
+      <img src="../../../public/imagenes/chip-profesionales.png" alt="" />
+      <img src="../../../public/imagenes/chip-profesionales.png" alt="" />
+      <img src="../../../public/imagenes/chip-profesionales.png" alt="" />
+      <img src="../../../public/imagenes/chip-profesionales.png" alt="" />
+    </div>
+  );
+};
