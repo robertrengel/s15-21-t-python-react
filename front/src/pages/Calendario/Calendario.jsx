f49740d3eb73141d58f04a582de1dd133b4287f9
Piero Ramirez <estibensmax2022@gmail.com>
2024-06-12 19:52:38 -0500

    Arreglando el error de la importación del Button

--- front/src/pages/Calendario/Calendario.jsx
+++ front/src/pages/Calendario/Calendario.jsx
@@ -1,7 +1,7 @@
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import styles from './Calendario.module.scss';
-import { Button } from '../Button/Button';
+import { Button } from "../../components/Button/Button";
 
 const Calendario = () => {
   return (
